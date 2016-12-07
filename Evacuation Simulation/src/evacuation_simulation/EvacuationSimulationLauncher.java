abfd647a7827cd57b5d6db28dffca78f7da9a809
Gil Domingues <gmdz95@gmail.com>
2016-12-07 14:47:00 +0000

    merge fixed

--- Evacuation Simulation/src/evacuation_simulation/EvacuationSimulationLauncher.java
+++ Evacuation Simulation/src/evacuation_simulation/EvacuationSimulationLauncher.java
@@ -21,8 +21,8 @@ import sajas.wrapper.ContainerController;
 
 public class EvacuationSimulationLauncher extends RepastSLauncher{
 
-	private static int N_KNOWLEDGEABLE = 1;
-	private static int N_INDEPENDENT = 1;
+	private static int N_KNOWLEDGEABLE = 0;
+	private static int N_INDEPENDENT = 4;
 	private static int N_INDEPENDENT_KNOWLEDGEABLE = 0;
 	private static int N_DEPENDENT_UNKNOWLEDGEABLE = 0;
 	private static int N_SECURITY_OFFICER = 0;
